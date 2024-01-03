b078125be504d93ed4b632783d762976e4354e62
Roger <a0952288480@gmail.com>
2024-01-03 17:57:41 +0800

    新增享元模式

---
+++ AddFlyweightPattern/Forest.cs
@@ -0,0 +1,38 @@
+﻿namespace AddFlyweightPattern
+{
+    /// <summary>
+    /// 森林 包含多個 Tree
+    /// </summary>
+    public class Forest
+    {
+        public List<Tree> trees = new();
+
+        /// <summary>
+        /// 種樹 透過 Factory 取得 TreeType
+        /// 生成 Tree 後存入 trees
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="name"></param>
+        /// <param name="color"></param>
+        /// <param name="texture"></param>
+        public void PlantTree(int x, int y, string name, string color, string texture)
+        {
+            TreeType type = TreeFactory.GetTreeType(name, color, texture);
+            Tree tree = new(x, y, type);
+            trees.Add(tree);
+        }
+
+        /// <summary>
+        /// 將森林中所有 Tree 繪製到 繪布 上
+        /// </summary>
+        /// <param name="canvas">繪布</param>
+        public void Draw(string canvas)
+        {
+            foreach(Tree tree in trees)
+            {
+                tree.Draw(canvas);
+            }
+        }
+    }
+}
